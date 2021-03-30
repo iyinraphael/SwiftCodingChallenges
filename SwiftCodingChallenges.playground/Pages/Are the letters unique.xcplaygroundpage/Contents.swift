4c68760d7f2258f0496a0e8dbfe818b7c7566b08
iyinraphael <iyinraphael@gmail.com>
2021-03-30 09:37:33 -0700

    string check for duplicates

--- SwiftCodingChallenges.playground/Pages/Are the letters unique.xcplaygroundpage/Contents.swift
+++ SwiftCodingChallenges.playground/Pages/Are the letters unique.xcplaygroundpage/Contents.swift
@@ -2,6 +2,35 @@
 
 import Foundation
 
-var str = "Hello, playground"
+// MARK: - Instruction
+/** Write a function that accepts a String as its only parameter, and returns true if the string has
+ only unique letters, taking letter case into account.*/
+
+// MARK: - Solution
+func challenge1(input: String) -> Bool {
+    var count = 0
+    let array = Array(input)
+    while count < array.count - 1 {
+        if array[count] == array[count + 1] {
+            return false
+        }
+        count += 1
+    }
+    return true
+}
+
+challenge1(input: "No duplicates")
+challenge1(input: "abcdefghijklmnopqrstuvwxyz")
+challenge1(input: "AaBbCc")
+challenge1(input: "Hello, world")
+
+// MARK: - Performant Solution
+func challenge1a(input: String) -> Bool {
+    return Set(input).count == input.count
+}
+challenge1a(input: "No duplicates")
+challenge1a(input: "abcdefghijklmnopqrstuvwxyz")
+challenge1a(input: "AaBbCc")
+challenge1a(input: "Hello, world")
 
 //: [Next](@next)
